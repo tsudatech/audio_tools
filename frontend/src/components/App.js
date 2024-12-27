a79b879b9ba035f091ea745127b165e4fc8a62d6
tsudatech <tsd.tch@gmail.com>
2024-12-28 02:05:02 +0900

    tailwind導入

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,45 +1,164 @@
 // App.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import UsersList from "./UsersList";
 import "../index.css";
 
 function App() {
   return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">Hello Tailwind CSS!</h1>
-      <p className="m-0 text-gray-400">Tailwind CSSです</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">
-        ボタン
-      </button>
+    <div className="">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a href="/">Home</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a href="/" className="btn btn-ghost text-2xl">
+            Youtube Pitch Shifter
+          </a>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+
+      {/* URL */}
+      <div className="container w-full mt-16">
+        <div className="card bg-neutral text-neutral-content w-full container pt-4 pb-4">
+          <p className="text font-bold">Please paste YouTube URL or Video ID</p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-2xl mt-2"
+          />
+          <div className="mt-2 flex space-x-4">
+            <select className="select select-accent w-full max-w-xs">
+              <option disabled selected>
+                Select pitch...
+              </option>
+              <option>-6</option>
+              <option>-5</option>
+              <option>-4</option>
+              <option>-3</option>
+              <option>-1</option>
+              <option>+1</option>
+              <option>+2</option>
+              <option>+3</option>
+              <option>+4</option>
+              <option>+5</option>
+              <option>+5</option>
+            </select>
+            <button className="btn btn-accent">Start Shift</button>
+          </div>
+        </div>
+      </div>
+
+      {/* 説明文 */}
+      <div className="container mt-16 justify-start items-center">
+        <div class="max-w-2xl">
+          <div>
+            <h1 class="text text-2xl">Markdown Style Title</h1>
+            <p className="mt-4">
+              LoopTube is a free online tool to repeat any YouTube videos. Just
+              select YouTube videos by typing a URL in the search bar, and you
+              can set AB loop in any point of the video. This is useful when you
+              want to learn some kind of skills (such as languages, sports,
+              music, etc.) by watching a specific part over and over.
+            </p>
+          </div>
+          <div class="mt-8">
+            <h1 class="text text-2xl">Markdown Style Title</h1>
+            <ul class="list-disc pl-5 text mt-4">
+              <li>
+                <strong>項目 1:</strong> これは最初のリストアイテムの説明です。
+              </li>
+              <li>
+                <strong>項目 2:</strong> 次の項目は追加情報を含みます。
+              </li>
+              <li>
+                <strong>項目 3:</strong> さらに多くのデータをここに記載します。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="footer footer-center bg-accent text-primary-content p-10 fixed bottom-0">
+        <aside>
+          <p className="font-bold">
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+        </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </div>
+        </nav>
+      </footer>
     </div>
   );
 }
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div>
-//         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/users">Users</Link>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         <Routes>
-//           <Route path="/users" element={<UsersList />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
 
 const container = document.getElementById("app");
 const root = createRoot(container);
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      flex: true,
+      justifyCenter: true,
+      padding: "2rem",
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
