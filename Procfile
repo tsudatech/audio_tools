57f19a1ffe7f7f16d3d51d3c67e500258e568b02
tsudatech <tsd.tch@gmail.com>
2024-12-31 21:54:43 +0900

    ディレクトリ名変更

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: gunicorn pitchshifter.wsgi --log-file -
+web: gunicorn audio_tools.wsgi --log-file -
 react-build: npm install --prefix frontend
 react-build: npm run build --prefix frontend
 release: python manage.py migrate
--- audio_tools/wsgi.py
+++ audio_tools/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pitchshifter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audio_tools.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
