c011ff5d246fa5df1669fc4da7e6643b081e6dc2
tsudatech <tsd.tch@gmail.com>
2025-01-16 13:28:41 +0900

    Description追加

---
+++ frontend/src/components/chordProgressionManager/Description.js
@@ -0,0 +1,50 @@
+// App.js
+import React from "react";
+
+function Description() {
+  return (
+    <div className="container justify-start items-center">
+      <div class="max-w-xl ml-2">
+        <div>
+          <h1 class="text text-2xl font-bold">
+            Welcome to Chord Progression Manager🎵
+          </h1>
+          <p className="mt-4">
+            Take your music composition to the next level with our powerful
+            chord progression management tool!
+          </p>
+          <p className="mt-4">Key Features:</p>
+          <ul class="list-disc pl-5 text mt-4">
+            <li>
+              All-in-One Management: Organize and manage all your chord
+              progressions in one convenient place.
+            </li>
+            <li>
+              Flexible Editing: Easily move and adjust chords across different
+              progressions to refine your creations.
+            </li>
+            <li>
+              Real-Time Playback: Instantly test your progressions by playing
+              them directly within the tool.
+            </li>
+            <li>
+              Save and Restore Anytime: Save your progressions to Cookies or
+              export them as JSON files for quick restoration.
+            </li>
+            <li>
+              MIDI File Export: Export your progressions as MIDI files and
+              seamlessly integrate them into your favorite DAW or music
+              software.
+            </li>
+          </ul>
+        </div>
+        <p class="mt-4">
+          Start creating and managing your chord progressions effortlessly
+          today! 🚀
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Description;
