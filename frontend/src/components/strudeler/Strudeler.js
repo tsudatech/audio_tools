49521776e04f48fa9272d935354e5d6f8902b384
tsudatech <tsd.tch@gmail.com>
2025-07-30 00:31:43 +0900

    generateId移動

--- frontend/src/components/strudeler/Strudeler.js
+++ frontend/src/components/strudeler/Strudeler.js
@@ -18,6 +18,17 @@ import SortableCodeBlock from "./SortableCodeBlock";
 import "./strudel/repl/repl-component.mjs";
 import { initAudioOnFirstClick } from "@strudel/webaudio";
 
+// ランダムID生成（12桁英数字）
+function generateId(len = 12) {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
 function Strudeler() {
   // Data State
   const [jsonData, setJsonData] = useState({});
@@ -481,17 +492,6 @@ function Strudeler() {
     // Monacoエディタの選択をリセット
     setSelectedCodeId(null);
     setSelectedCode("");
-  }
-
-  // ランダムID生成（12桁英数字）
-  function generateId(len = 12) {
-    const chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    for (let i = 0; i < len; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
   }
 
   // コード複製
