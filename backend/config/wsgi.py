2964437a03b8a2dd79581c9a441ec8087069c64a
tsudatech <tsd.tch@gmail.com>
2025-08-18 20:17:44 +0900

    refactor: restructure Django project by updating settings and paths

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "audio_tools.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
